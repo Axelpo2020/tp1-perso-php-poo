63ebd6d44fd67f34d9d2ad43d314651e15c892d8
Axel <axelpo@free.fr>
2020-10-07 09:41:44 +0200

    initial commit

---
+++ Personnage.php
@@ -0,0 +1,18 @@
+<?php
+class Personnage {
+    private $_id,
+    $_nom,
+    $_degats;
+
+    public function frapper(Personnage $perso)
+    {
+        if () {
+            echo 'Vous vous frapper vous meme !';
+        }
+
+    }
+    public function recevoirdegat()
+    {
+        
+    }
+}
